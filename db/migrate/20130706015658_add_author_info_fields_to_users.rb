21c8456ce094faaa318d05603939ad51d941f090
alex <alesssandro@ukr.net>
2014-02-11 15:20:25 +0200

    rewrite migration add_author_info_fields_to_users because it was not reverting

--- db/migrate/20130706015658_add_author_info_fields_to_users.rb
+++ db/migrate/20130706015658_add_author_info_fields_to_users.rb
@@ -1,10 +1,8 @@
 class AddAuthorInfoFieldsToUsers < ActiveRecord::Migration
   def change
-    change_table :spree_users do |t|
-      t.string :nickname
-      t.string :website_url
-      t.string :google_plus_url
-      t.text :bio_info
-    end
+    add_column :spree_users, :nickname, :string
+    add_column :spree_users, :website_url, :string
+    add_column :spree_users, :google_plus_url, :string
+    add_column :spree_users, :bio_info, :text
   end
 end
