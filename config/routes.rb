002d5c78f8c096be922aea5ae6504fcf4b382422
Thomas Winkler <tom@formrausch.com>
2014-04-21 11:26:58 +0200

    use rails 4 routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,13 +3,13 @@ Spree::Core::Engine.routes.draw do
     resources :blog_entries
   end
   scope Spree::Config['blog_alias'], as: 'blog' do
-    match '/tag/:tag', :to => 'blog_entries#tag', :as => :tag
-    match '/category/:category', :to => 'blog_entries#category', :as => :category
-    match '/author/:author', :to => 'blog_entries#author', :as => :author
-    match '/:year/:month/:day/:slug', :to => 'blog_entries#show', :as => :entry_permalink
-    match '/:year(/:month)(/:day)', :to => 'blog_entries#archive', :as => :archive,
+    get '/tag/:tag', :to => 'blog_entries#tag', :as => :tag
+    get '/category/:category', :to => 'blog_entries#category', :as => :category
+    get '/author/:author', :to => 'blog_entries#author', :as => :author
+    get '/:year/:month/:day/:slug', :to => 'blog_entries#show', :as => :entry_permalink
+    get '/:year(/:month)(/:day)', :to => 'blog_entries#archive', :as => :archive,
           :constraints => {:year => /(19|20)\d{2}/, :month => /[01]?\d/, :day => /[0-3]?\d/}
-    match '/feed', :to => 'blog_entries#feed', :as => :feed, :format => :rss
-    match '/', :to => 'blog_entries#index'
+    get '/feed', :to => 'blog_entries#feed', :as => :feed, :format => :rss
+    get '/', :to => 'blog_entries#index'
   end
 end
