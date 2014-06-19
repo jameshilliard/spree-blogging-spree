ca84f023de28825c55d399a94a1a4c1ac64eba90
James Hilliard <james.hilliard1@gmail.com>
2014-06-19 01:46:17 -0600

    refactor override

--- app/overrides/add_blog_to_admin_configuration_sidebar.rb
+++
@@ -1,5 +0,0 @@
-Deface::Override.new(virtual_path: "spree/admin/shared/_configuration_menu",
-                     name: "blog_admin_configurations_menu",
-                     insert_bottom: "[data-hook='admin_configurations_sidebar_menu'], #admin_configurations_sidebar_menu[data-hook]",
-                     text: "<%= configurations_sidebar_menu_item t(:blog_entries), admin_blog_entries_path %>",
-                     disabled: false)
