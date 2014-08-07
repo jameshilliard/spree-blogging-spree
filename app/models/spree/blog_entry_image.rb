f32624b7550862b20272d6075b573f3d5a8f1df6
James Hilliard <james.hilliard1@gmail.com>
2014-08-06 23:14:29 -0600

    add normal size square image

--- app/models/spree/blog_entry_image.rb
+++ app/models/spree/blog_entry_image.rb
@@ -1,6 +1,6 @@
 class Spree::BlogEntryImage < Spree::Image
   has_attached_file :attachment, 
-     :styles => {:mini => '48x48#', :small => '100x150>', :normal => '200x200>', :largesquare => '600x600#', :large => '600x600>'},
+     :styles => {:mini => '48x48#', :small => '100x150>', :normalsquare => '200x200#', :normal => '200x200>', :largesquare => '600x600#', :large => '600x600>'},
      :default_style => :large,
      :url => "/assets/blog_entry_images/:id/:style/:basename.:extension",
      :path => ":rails_root/public/assets/blog_entry_images/:id/:style/:basename.:extension"  
